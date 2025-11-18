367e6da786618d7ed0e4fb78ca8523c93d63338d
CodeTechNL <65519422+CodeTechNL@users.noreply.github.com>
2025-11-18 12:11:04 +0100

    Updates

--- cdk/lib/constructs/CookieScanner/Lambda/CookieScannerLambdaContainerResource.ts
+++ cdk/lib/constructs/CookieScanner/Lambda/CookieScannerLambdaContainerResource.ts
@@ -15,24 +15,23 @@ export class CookieScannerLambdaContainerResource extends DockerImageFunction {
     constructor(scope: Construct, id: string, props: CookieScannerLambdaContainerProps) {
         const {eventSource, eventDetail, bus} = props;
 
+
         const baseProps = {
             code: DockerImageCode.fromImageAsset(fromRoot("cookie-scanner"), {
                 file: "Dockerfile",
                 platform: Platform.LINUX_AMD64,
             }),
+            // code: DockerImageCode.fromEcr('cdk-hnb659fds-container-assets-585008041582-eu-west-3', {
+            //
+            // }),
             timeout: Duration.minutes(15),
             memorySize: 2048,
+            tagOrDigest: "latest",
             architecture: Architecture.X86_64,
             environment: {
                 EVENT_SOURCE_NAME: eventSource,
                 EVENT_DETAIL_TYPE: eventDetail,
                 EVENT_BUS_NAME: bus,
-                HOME: "/tmp/chrome-home",
-                XDG_RUNTIME_DIR: "/tmp/runtime-dir",
-                XDG_CONFIG_HOME: "/tmp/chrome-home/.config",
-                XDG_CACHE_HOME: "/tmp/chrome-home/.cache",
-                XDG_DATA_HOME: "/tmp/chrome-home/.local/share",
-                DBUS_SESSION_BUS_ADDRESS: "/dev/null",
             }
         };
 
